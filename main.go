b47321c8baa72fea961c505021a8567bf361bb1b
chenkun <1621454839@qq.com>
2022-03-10 20:41:38 +0800

    style: add golingci-lint config file

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+)
+
 func main() {
-	
+	b := make([]string, 10)
+	for i := 0; i < 1000; i++ {
+		s := strconv.Itoa(i)
+		b = append(b, s)
+	}
+	fmt.Println(b[0])
+	// fbasfjqwlhefqwlefhbqhjewfgbejsfabgdhbgsdhffasfgbasdhgalsjfhajklsfhaqjskfghasjldghsdfbhsghkasdgksdghgasghdahskfgqkgqyhghka
+	//fmt.Println("fbasfjqwlhefqwlefhbqhjewfgbejsfabgdhbgsdhfgaskhgshagshdghsfghasfjaksgk", b[0])
 }
